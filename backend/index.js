8d382f24d986b17d0c3e6c610a3642f4a61a28dd
Anand-Aditya-23 <anand77aditya@gmail.com>
2026-03-17 21:56:07 +0530

    feat: connect MongoDB Atlas cloud database

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
